f177858e623427d496617d24eed04ca6a5272ad9
Darius Daskevicius <darek.daskevic@gmail.com>
2020-11-13 15:38:34 +0200

    Model View Template and logic which cuts to big images added

--- Shop/mainapp/admin.py
+++ Shop/mainapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.forms import ModelChoiceField, ModelForm, ValidationError
 from .models import *
-from PIL import Image
 from django.utils.safestring import mark_safe
 
 class IPhoneAdminForm(ModelForm):
@@ -9,23 +8,25 @@ class IPhoneAdminForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['image'].help_text = mark_safe(
-            '<span style="color:red; font-size:14px;">Add an image at least {}x{}</span>'.format(
-                *Product.MIN_RESOLUTION
+            """<span style="color:red; font-size:14px;"> When uploading an image with a resolution greater than {}x{}, it will be cropped</span>
+            """.format(
+                *Product.MAX_RESOLUTION
             )
         )
-
-    def clean_image(self):
-        image = self.cleaned_data['image']
-        img = Image.open(image)
-        min_height, min_width = Product.MIN_RESOLUTION
-        max_height, max_width = Product.MAX_RESOLUTION
-        if image.size > Product.MAX_IMAGE_SIZE:
-            raise ValidationError('Image size should not exceed 3MB')
-        if img.height < min_height or img.width < min_width:
-            raise ValidationError('Image resolution is less than minimum')
-        if img.height > max_height or img.width > max_width:
-            raise ValidationError('Image resolution is more than maximum')
-        return image
+    # Commented code works only with models.Product.save commented part of code
+    # from PIL import Image -> import this if you want to work with commented part
+    # def clean_image(self):
+    #     image = self.cleaned_data['image']
+    #     img = Image.open(image)
+    #     min_height, min_width = Product.MIN_RESOLUTION
+    #     max_height, max_width = Product.MAX_RESOLUTION
+    #     if image.size > Product.MAX_IMAGE_SIZE:
+    #         raise ValidationError('Image size should not exceed 3MB')
+    #     if img.height < min_height or img.width < min_width:
+    #         raise ValidationError('Image resolution is less than minimum')
+    #     if img.height > max_height or img.width > max_width:
+    #         raise ValidationError('Image resolution is more than maximum')
+    #     return image
 
 class IPhoneAdmin(admin.ModelAdmin):
 
--- Shop/mainapp/models.py
+++ Shop/mainapp/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+import sys
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from io import BytesIO
 from PIL import Image
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.urls import reverse
 
-user = get_user_model()
+User = get_user_model()
+
+def get_product_url(obj, viewname):
+    ct_model = obj.__class__.meta.model.name
+    return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
 
 class MinResolutionErrorException(Exception):
     pass
@@ -61,15 +69,28 @@ class Product(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
+        # image = self.image
+        # img = Image.open(image)
+        # min_height, min_width = self.MIN_RESOLUTION
+        # max_height, max_width = self.MAX_RESOLUTION
+        # if img.height < min_height or img.width < min_width:
+        #     raise MinResolutionErrorException('Image resolution is less than minimum')     // commented side bock imgage bigger than 800x800
+        # if img.height > max_height or img.width > max_width:
+        #     raise MaxResolutionErrorException('Image resolution is more than maximum')
+        # return image
         image = self.image
         img = Image.open(image)
-        min_height, min_width = self.MIN_RESOLUTION
-        max_height, max_width = self.MAX_RESOLUTION
-        if img.height < min_height or img.width < min_width:
-            raise MinResolutionErrorException('Image resolution is less than minimum')
-        if img.height > max_height or img.width > max_width:
-            raise MaxResolutionErrorException('Image resolution is more than maximum')
-        return image
+        new_img = img.convert('RGB')
+        resized_new_image = new_img.resize((800, 800), Image.ANTIALIAS)
+        filestream = BytesIO()
+        resized_new_image.save(filestream, 'JPEG', quality=90)                      ## This code crops image if it is  bigger then 800x800
+        filestream.seek(0)
+        name = '{}.{}'.format(*self.image.name.split('.'))
+        self.image = InMemoryUploadedFile(
+            filestream, 'ImageField', name, 'jpeg/image', sys.getsizeof(filestream), None
+        )
+
+        super().save(*args, **kwargs)
 
 
 class Iphone(Product):
@@ -85,6 +106,9 @@ class Iphone(Product):
     def __str__(self):
         return '{} : {}'.format(self.category.name, self.title)
 
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
+
 class Watch(Product):
 
     model = models.CharField(max_length=100, verbose_name='Apple Watch')
@@ -96,9 +120,12 @@ class Watch(Product):
     def __str__(self):
         return '{} : {}'.format(self.category.name, self.title)
 
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
+
 class CartProduct(models.Model):
 
-    user = models.ForeignKey('Customer', verbose_name='Customer', on_delete=models.CASCADE)
+    User = models.ForeignKey('Customer', verbose_name='Customer', on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name='Cart', on_delete=models.CASCADE, related_name='related_products')
     contentType = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     objectId = models.PositiveIntegerField()
@@ -121,7 +148,7 @@ class Cart(models.Model):
 
 class Customer(models.Model):
 
-    user = models.ForeignKey(user, verbose_name='User', on_delete=models.CASCADE)
+    User = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
     phone = models.CharField(max_length=13, verbose_name='Phone number')
     adress = models.CharField(max_length=255, verbose_name='Address')
 
